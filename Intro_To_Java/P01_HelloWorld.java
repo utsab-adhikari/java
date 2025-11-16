0e530433535fbd1fcc361dbfeca9949277301300
Utsab <utsab1adhikari@gmail.com>
2025-11-16 21:55:05 +0545

    First Commit && Hello world

---
+++ Intro_To_Java/P01_HelloWorld.java
@@ -0,0 +1,8 @@
+// Hello World - The First Code
+public class P01_HelloWorld
+     {
+        public static void main(String a[]) {
+            System.out.println("Hello World");
+
+        }
+     }
