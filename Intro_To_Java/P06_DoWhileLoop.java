6ded1d5758414aa94d2930e6c8c5b2d967e1b806
Utsab <utsab1adhikari@gmail.com>
2025-11-17 21:55:57 +0545

    P06 DoWhileLoop added

---
+++ Intro_To_Java/P06_DoWhileLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P06_DoWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        
+        do {
+            System.out.println("Hello "+name+" "+num);
+            num++;
+        } while(num<5);
+        
+        
+        System.out.println("Do-While Loop Ends");
+       
+    }
+}
