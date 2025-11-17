61fda9039b21fa420139c8c85f72293af4a071e8
Utsab <utsab1adhikari@gmail.com>
2025-11-17 21:58:50 +0545

    P08 ForLoop added

---
+++ Intro_To_Java/P08_ForLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P08_ForLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+        
+        for(int i=0;i<num;i++) {
+            System.out.println("Hello "+name+" "+i);
+        }
+        
+        
+        System.out.println("For Loop Ends");
+       
+    }
+}
