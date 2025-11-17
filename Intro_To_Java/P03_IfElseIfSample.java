4b9a9f3fbaed203d01e606ab7f70c312eca49db0
Utsab <utsab1adhikari@gmail.com>
2025-11-17 06:54:31 +0545

    P03-ifelsesample added

---
+++ Intro_To_Java/P03_IfElseIfSample.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P03_IfElseIfSample {
+    public static void main(String a[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Age: ");
+        int Age = sc.nextInt();
+        
+        if(Age >= 18) {
+            System.out.println("Allowed for voting");
+        } else if(Age <18) {
+            System.out.println("You Cannot Vote");
+        } else {
+            System.out.println("Try Again");
+        }
+        
+    }
+} 
