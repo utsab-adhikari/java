6d687bb70aa8a42a4dd36731d099f45a69e4f369
Utsab <utsab1adhikari@gmail.com>
2025-11-17 22:01:29 +0545

    P09 ForEachLoop added

---
+++ Intro_To_Java/P09_ForEachLoop.java
@@ -0,0 +1,15 @@
+
+public class P09_ForEachLoop {
+    public static void main(String[] args) {
+       int i;
+       String week[] = {"Sun", "Mon", "Tue", "Thu", "Fri", "Sat"};
+       
+       for(String day: week) {
+           System.out.println(day);
+       }
+       
+       System.out.println("For Loop Ends");
+       
+    }
+}
+
