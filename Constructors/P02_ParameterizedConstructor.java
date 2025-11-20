167e62f32785ae066191780e6b5dcb299d041e67
Utsab <utsab1adhikari@gmail.com>
2025-11-20 21:58:04 +0545

    Constructor: P02 Parameterized added

---
+++ Constructors/P02_ParameterizedConstructor.java
@@ -0,0 +1,23 @@
+package Constructors;
+
+class car {
+    private String model;
+    private String color;
+
+    car(String m, String c) {
+        model = m;
+        color = c;
+    }
+    public void showInfo() {
+        System.out.println("Model: "+model);
+        System.out.println("Color: "+color);
+    }
+}
+
+public class P02_ParameterizedConstructor {
+    public static void main(String[] args) {
+        car car1 = new car("Lambo", "Black");
+        // constructor called;
+        car1.showInfo();
+    }
+}
