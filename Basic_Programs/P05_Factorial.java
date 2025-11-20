e48bc83a1560068b3730f3c6a8f72893ec25556b
Utsab <utsab1adhikari@gmail.com>
2025-11-20 22:10:27 +0545

    Basic_Programs: P05 Factorial added

---
+++ Basic_Programs/P05_Factorial.java
@@ -0,0 +1,20 @@
+// factorial by for loop
+package Basic_Programs;
+
+import java.util.Scanner;
+
+public class P04_Factorial {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number:");
+        int num = sc.nextInt();
+
+        int temp = num;
+        int i, fact = 1;
+        for (i = num; i > 0; i--) {
+            fact = fact * i;
+        }
+            System.out.println("The Factorial of Number " + temp + " is "+ fact);
+        
+    }
+}
