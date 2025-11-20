b08aa86a07f3dff7aaa4c995f8cd30ee9eb77fca
Utsab <utsab1adhikari@gmail.com>
2025-11-20 21:55:48 +0545

    Constructor: P01 Default added

---
+++ Constructors/P01_DefaultConstructor.java
@@ -0,0 +1,23 @@
+package Constructors;
+
+class car {
+    private String model;
+    private String color;
+
+    car() {
+        model = "BMW";
+        color = "Red";
+    }
+    public void showInfo() {
+        System.out.println("Model: "+model);
+        System.out.println("Color: "+color);
+    }
+}
+
+public class P01_DefaultConstructor {
+    public static void main(String[] args) {
+        car car1 = new car();
+        // constructor called;
+        car1.showInfo();
+    }
+}
