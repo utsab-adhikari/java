45e22f98d9990c56b9dd04cbe3a9bc4af4435527
Utsab <utsab1adhikari@gmail.com>
2025-11-24 20:39:43 +0545

    Exception Handling: P02 try-multiple-catch added

--- Exception_Handling/P02_TryMultipleCatch.java
+++ Exception_Handling/P02_TryMultipleCatch.java
@@ -1,21 +1,27 @@
+
 // Basic Example of Try and catch for exception handling;
 
 package Exception_Handling;
 
-public class P01_TryCatch
+public class P02_TryMultipleCatch
 {
 	public static void main(String[] args) {
-		int a = 0;
+		int a = 1;
 		int b = 0;
+        int num[] = {1, 2, 3, 4, 5};
 
 		try {
-		    b = 10/a; // divide by zero : exception
+		    b = 10/a; 
+            System.err.println(num[7]);
 		    System.out.println("Hey There, from try block");
 
-		} catch (Exception e) {
+		} 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of stack, "+e);
+
+        } catch (Exception e) {
 		    System.out.println("Something went wrong, "+e);
-            // exception will be handled here
-		}
+		} 
 
 		System.out.println(" we got b = "+b);
 		System.out.println("Bye there !");
