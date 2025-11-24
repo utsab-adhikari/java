ec9b051ac85fb79162ac991d6e83f4000dd80540
Utsab <utsab1adhikari@gmail.com>
2025-11-24 22:11:12 +0545

    Exception Handling: P03 throw added

---
+++ Exception_Handling/P03_Throw.java
@@ -0,0 +1,23 @@
+package Exception_Handling;
+
+// throw is used to throw new exception if we want 
+
+public class P03_Throw {
+    public static void main(String[] args) {
+        {
+            int i=20;
+            int j=0;
+            try {
+
+                if(j==0) {
+                    throw new ArithmeticException("Message Will be here");
+                }
+            } catch(ArithmeticException e) {
+                j = 100;
+                System.err.println("Cannot divide "+e);
+            }
+            System.err.println(j);
+            System.err.println("Ends here");
+        }
+    }
+}
