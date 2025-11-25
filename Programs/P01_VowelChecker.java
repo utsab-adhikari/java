7f9bbc4e96a2d9ff3b8f69d3f74c6677a5a728af
Utsab <utsab1adhikari@gmail.com>
2025-11-25 20:59:22 +0545

    Programs: P01 Vowel Checker

---
+++ Programs/P01_VowelChecker.java
@@ -0,0 +1,39 @@
+// Simple Vowel Checker Program in Java
+package Programs;
+
+import java.util.Scanner;
+
+public class P01_VowelChecker {
+    
+    public static void main(String arr[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter Alphabet: ");
+        String alphabet = sc.next();
+        
+        switch(alphabet) {
+            case "a": 
+                System.out.println("Yes the alphabet is Vowel");
+                break;
+                
+            case "e": 
+                System.out.println("Yes the alphabet is Vowel");
+                break;
+                
+            case "i": 
+                System.out.println("Yes the alphabet is Vowel");
+                break;
+                
+            case "u": 
+                System.out.println("Yes the alphabet is Vowel");
+                break;
+                    
+            default : 
+                System.out.println("The Alphabet is Consonant");
+               }
+         
+        
+        
+        
+    }
+    
+}
