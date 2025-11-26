d1e7a0dc543ed73b23de9f2d6cfea10bb5c3a6e9
Utsab <utsab1adhikari@gmail.com>
2025-11-26 21:56:36 +0545

    Programs: P03_PrimeOrNot added

---
+++ Programs/P03_PrimeOrNot.java
@@ -0,0 +1,27 @@
+// Write a program to input a number and display whether it is prime or not.
+package Programs;
+
+import java.util.Scanner;
+
+public class P03_PrimeOrNot {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number:");
+        int num = sc.nextInt();
+
+        int temp = num;
+        int i;
+        for (i = 2; i < num; i++) {
+            if (num % i == 0) {
+                break;
+            }
+        }
+
+        if (temp == i) {
+            System.out.println("The Number " + temp + " is Prime.");
+        } else {
+            System.out.println("The Number " + temp + " is Not a Prime");
+        }
+        sc.close();
+    }
+}
