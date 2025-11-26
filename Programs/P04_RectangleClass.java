ec9d3fd287c15d34dae097dcdd5aa131965f56d8
Utsab <utsab1adhikari@gmail.com>
2025-11-26 21:59:54 +0545

    Programs: P04_RectangleClass added

---
+++ Programs/P04_RectangleClass.java
@@ -0,0 +1,34 @@
+/*Write a java program to create Rectangle class with data member length and breadth. Include
+methods getData() and displayArea() in the class. Finally create an object of Rectangle class
+and display its area. */
+package Programs;
+
+import java.util.Scanner;
+
+class Rectangle {
+
+    int length;
+    int breadth;
+
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        length = sc.nextInt();
+        System.out.print("Enter breadth: ");
+        breadth = sc.nextInt();
+        sc.close();
+    }
+
+    void displayArea() {
+        int area = length * breadth;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
+public class P04_RectangleClass {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        rect.getData();
+        rect.displayArea();
+    }
+}
