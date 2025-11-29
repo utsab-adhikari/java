fae54c65782543b58259e669704e68bb15fd3605
Utsab <utsab1adhikari@gmail.com>
2025-11-29 22:15:49 +0545

    Programs: Lab01 P07 Basic pattern 02

---
+++ Programs/Lab01/P07_Pattern02.java
@@ -0,0 +1,34 @@
+// Write a java program to print the following pattern
+
+package Programs.Lab01;
+
+public class P07_Pattern02 {
+    public static void main(String arr[]) {
+        for(int i=0;i<10;i++) {
+            for(int k=0; k<i; k++){
+                System.out.print(" ");
+            }
+            for(int j=10;j>i;j--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+// output:
+
+/*
+
+********** 
+ ********* 
+  ******** 
+   ******* 
+    ****** 
+     ***** 
+      **** 
+       *** 
+        ** 
+         * 
+
+ */
