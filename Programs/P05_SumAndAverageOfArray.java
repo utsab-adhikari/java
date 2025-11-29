31c18a2bbe87472795ba18842167a5f47bbdf430
Utsab <utsab1adhikari@gmail.com>
2025-11-29 22:11:14 +0545

    Proggrams sum and avg of array

---
+++ Programs/P05_SumAndAverageOfArray.java
@@ -0,0 +1,21 @@
+// Write a java program to find sum and average of all elements in an array.
+
+package Programs;
+
+public class P05_SumAndAverageOfArray {
+  
+    public static void main(String arr[]) {
+        int sum=0, avg;
+        int array[] = {10, 20, 30, 40, 50};
+        
+        for(int num: array) {
+            sum = sum + num;
+        }
+        int n = array.length;
+        avg = sum/n;
+        
+        System.out.println("Total: "+sum);
+        System.out.println("Average: "+avg);
+    }
+}
+
