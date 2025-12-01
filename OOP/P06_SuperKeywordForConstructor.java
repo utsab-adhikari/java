2d96b544a4738a173d7eed16520ca875eff3e2ce
Utsab <utsab1adhikari@gmail.com>
2025-12-01 21:49:07 +0545

    OOP: P06 Super Keyword used to call parent constructor added

---
+++ OOP/P06_SuperKeywordForConstructor.java
@@ -0,0 +1,33 @@
+// Super Keyword for Constructor Example in Java
+
+package OOP;
+
+class Len {
+    int length;
+    Len(int l) {
+        length = l;
+    }
+}
+
+class B extends Len {
+    int breadth;
+    B(int l, int b) {
+        super(l);
+        // for constructor call of parent
+        breadth = b;
+    }
+    
+    void area() {
+        int a = length * breadth;
+        System.out.println("Area = "+a);
+    }
+}
+
+public class P06_SuperKeywordForConstructor {
+    public static void main (String[] args) {
+        B obj = new B(4, 5);
+        obj.area();
+        
+    }
+}
+
