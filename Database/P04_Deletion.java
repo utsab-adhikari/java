71cc3540c167d70260af07c17f57b83eed5466cc
Utsab <utsab1adhikari@gmail.com>
2026-01-08 22:59:55 +0545

    Database data Deletion

--- Database/P04_Deletion.java
+++ Database/P04_Deletion.java
@@ -4,28 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class P01_Connection {
-
+public class P04_Deletion {
     public static void main(String arr[]) {
-        getConnection();
+        deleteData();
     }
-
     public static Connection getConnection() {
-
+        
         try {
             String driver = "com.mysql.cj.jdbc.Driver";
             String dbUrl = "jdbc:mysql://localhost:3306/NCITDB";
-            /*
-            here NCITDB is name of the Database created in MySQL Database (XAMPP)
-            */
             Class.forName(driver);
             Connection conn = DriverManager.getConnection(dbUrl, "root", "");
             System.out.println("Database Connected Successfully");
             return conn;
         } catch (Exception e) {
-            System.out.println("Connection Error: " + e);
+            System.out.println("Connection Error: "+e);
         }
         return null;
     }
-
+    
+    public static void deleteData() {
+        try{
+            Statement statement = getConnection().createStatement();
+            String query = "DELETE FROM ncit_table01 WHERE roll = 2";
+            
+            int result = statement.executeUpdate(query);
+            System.out.println(result+"row(s) Deleted from Database");
+        } catch(Exception e) {
+            System.out.println("Insertion Error: "+e);
+        }
+    }
 }
+
