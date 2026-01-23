0a96b752c561997bafe0dd65e4c763498d26657c
Utsab <utsab1adhikari@gmail.com>
2026-01-23 22:28:44 +0545

    Servlet example

---
+++ servlet/example.java
@@ -0,0 +1,16 @@
+package servlet;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class example extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
